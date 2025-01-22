a2700ad4c0358c8d1a5f2ba77bd769656e094237
Andrew Weist <andrew.weist@gmail.com>
2025-01-22 16:34:07 -0700

    fix tailwind css import

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,7 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
+import tailwindStyle from "./tailwind.css?url";
 import { Toaster, toast } from "sonner";
 
 export const links: LinksFunction = () => [
@@ -21,6 +21,10 @@ export const links: LinksFunction = () => [
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+  },
+  {
+    rel: "stylesheet",
+    href: tailwindStyle,
   },
 ];
 
